b4d3abfe94daf0b0686c6245ab3106dd25ba37dd
Michael Darko <michael.darko@amalitech.com>
2024-11-25 17:48:21 +0000

    add setup pages

---
+++ week_4/quiz-trivia-app/src/app/screens/finish-page/finish-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-finish-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './finish-page.component.html',
+  styleUrl: './finish-page.component.css'
+})
+export class FinishPageComponent {
+
+}
