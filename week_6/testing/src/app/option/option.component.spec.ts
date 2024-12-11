90e329a76e417c5a7a70b5e12d387c249167679c
Michael Darko <michael.darko@amalitech.com>
2024-12-11 14:17:23 +0000

    duplicate quiz app

---
+++ week_6/testing/src/app/option/option.component.spec.ts
@@ -0,0 +1,12 @@
+import { OptionComponent } from './option.component';
+
+describe('optionsComponent', () => {
+
+  it('Should display', () => {
+    expect(OptionComponent).toBeDefined();
+  });
+
+  it('Should show expected options', () => {
+
+  });
+});
