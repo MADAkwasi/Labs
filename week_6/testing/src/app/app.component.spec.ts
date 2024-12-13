a3b8853ee2005496f982b4942b3334d4655b53e1
Michael Darko <michael.darko@amalitech.com>
2024-12-13 08:52:36 +0000

    implement answer button tests

--- week_6/testing/src/app/app.component.spec.ts
+++ week_6/testing/src/app/app.component.spec.ts
@@ -1,16 +1,40 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { QuizPageComponent } from './screens/quiz-page/quiz-page.component';
+import { AnswerComponent } from './screens/quiz-page/answer/answer.component';
+import { OptionComponent } from './option/option.component';
 import { AppComponent } from './app.component';
 
-describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [AppComponent],
-    }).compileComponents();
+describe('App Component', () => {
+  let homePageFixture: ComponentFixture<AppComponent>;
+  let appComponent: AppComponent;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        AppComponent,
+        QuizPageComponent,
+        OptionComponent,
+        AnswerComponent,
+      ],
+    });
+
+    homePageFixture = TestBed.createComponent(AppComponent);
+    appComponent = homePageFixture.componentInstance;
+    homePageFixture.detectChanges();
   });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+  it('should render AnswerComponent when transitioning to QuizPageComponent', () => {
+    const button = homePageFixture.debugElement.query(By.css('.option')).nativeElement;
+    button.click();
+
+    appComponent.screen = 'active';
+    homePageFixture.detectChanges();
+
+    const quizPage = homePageFixture.debugElement.query(By.directive(QuizPageComponent));
+    expect(quizPage).toBeTruthy();
+
+    const answerComponent = quizPage.query(By.directive(AnswerComponent));
+    expect(answerComponent).toBeTruthy();
   });
 });
