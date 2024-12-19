f243a7d0db6aecd0d3e9aa54bd6c993e0ccba2e9
Michael Darko <michael.darko@amalitech.com>
2024-12-19 11:44:54 +0000

    feat(app): create filter button

---
+++ week_7/invoice-app/src/app/components/dropdown/dropdown.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { IconComponent } from "../icon/icon.component";
+
+@Component({
+  selector: 'app-dropdown',
+  standalone: true,
+  imports: [IconComponent],
+  templateUrl: './dropdown.component.html',
+  styleUrl: './dropdown.component.css'
+})
+export class DropdownComponent {
+
+}
--- week_7/invoice-app/src/app/components/headline/headline.component.ts
+++ week_7/invoice-app/src/app/components/headline/headline.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
-import { ButtonComponent } from "../button/button.component";
-import { IconComponent } from "../icon/icon.component";
+import { ButtonComponent } from '../button/button.component';
+import { IconComponent } from '../icon/icon.component';
+import { DropdownComponent } from "../dropdown/dropdown.component";
 
 @Component({
   selector: 'app-headline',
   standalone: true,
-  imports: [ButtonComponent, IconComponent],
+  imports: [ButtonComponent, IconComponent, DropdownComponent],
   templateUrl: './headline.component.html',
-  styleUrl: './headline.component.css'
+  styleUrl: './headline.component.css',
 })
 export class HeadlineComponent {
-
+  
 }
