25e1c93f39fb9146d8418a5a44ca561cebf35e2e
Michael Darko <michael.darko@amalitech.com>
2024-12-30 01:23:43 +0000

    refactor(app): adjust form and dialog component interaction layout

--- week_7/invoice-app/src/app/app.component.ts
+++ week_7/invoice-app/src/app/app.component.ts
@@ -1,17 +1,4 @@
-import {
-  AfterViewChecked,
-  AfterViewInit,
-  Component,
-  effect,
-  ElementRef,
-  HostListener,
-  inject,
-  OnDestroy,
-  OnInit,
-  Renderer2,
-  signal,
-  ViewChild,
-} from '@angular/core';
+import { Component, effect, inject, Renderer2 } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { CommonModule, ViewportScroller } from '@angular/common';
@@ -36,67 +23,27 @@ import { FormComponent } from './components/form/form.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent implements AfterViewChecked {
+export class AppComponent {
   private readonly store = inject(Store);
   private readonly viewportScroller = inject(ViewportScroller);
   private readonly renderer = inject(Renderer2);
-  private readonly elementRef = inject(ElementRef);
   wantsToDelete = this.store.selectSignal(selectDeleteState);
   isFormActive = this.store.selectSignal(selectFormState);
-  deleteState!: boolean;
-  formHeight!: number;
-  mainEL!: ElementRef;
-  appFormEl!: FormComponent;
 
   constructor() {
     effect(() => {
-      this.deleteState = this.wantsToDelete();
+      const deleteState = this.wantsToDelete();
+      const formState = this.isFormActive();
 
-      document.body.style.overflowY = this.deleteState ? 'hidden' : 'scroll';
-      this.viewportScroller.scrollToPosition([0, 0]);
-    });
-
-    effect(() => {
-      if (this.isFormActive()) {
-        const height = this.appFormEl;
-
-        this.renderer.setStyle(
-          this.renderer.selectRootElement('main', true),
-          'height',
-          height
-        );
+      if (deleteState || formState) {
+        this.renderer.setStyle(document.body, 'overflow', 'hidden');
       } else {
-        this.renderer.setStyle(
-          this.renderer.selectRootElement('main', true),
-          'height',
-          'auto'
-        );
+        this.renderer.setStyle(document.body, 'overflow', 'auto');
+      }
+
+      if (deleteState) {
+        this.viewportScroller.scrollToPosition([0, 0]);
       }
     });
-  }
-
-  ngAfterViewChecked(): void {
-    // Dynamically access 'main' and 'app-form' elements
-    this.mainEL = this.renderer.selectRootElement('main', true);
-    this.appFormEl = this.renderer.selectRootElement('app-form', true);
-
-    // Log the elements to check if they are available
-    console.log('Main Element:', this.mainEL);
-    console.log('App Form Element:', this.appFormEl);
-
-    // Access the height of the app-form element if it exists
-    if (this.appFormEl) {
-      //   this.formHeight = this.appFormEl.nativeElement.clientHeight;
-      //   console.log('Form Height:', this.formHeight);
-
-      console.log(this.appFormEl);
-    }
-  }
-
-  ngOnDestroy(): void {
-    // Reset height when component is destroyed
-    if (this.mainEL) {
-      this.renderer.setStyle(this.mainEL.nativeElement, 'height', 'auto');
-    }
   }
 }
