a6385c96aa1a710124374309c1ef8f0556a920cd
Michael Darko <michael.darko@amalitech.com>
2025-01-09 18:09:50 +0000

    feat(app): write tests for delete card component

--- week_7/invoice-app/src/app/components/delete-card/delete-card.component.spec.ts
+++ week_7/invoice-app/src/app/components/delete-card/delete-card.component.spec.ts
@@ -1,23 +1,59 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { DeleteCardComponent } from './delete-card.component';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+import { provideMockStore, MockStore } from '@ngrx/store/testing';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 describe('DeleteCardComponent', () => {
   let component: DeleteCardComponent;
   let fixture: ComponentFixture<DeleteCardComponent>;
+  let debugElement: DebugElement;
+  let store: MockStore;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [DeleteCardComponent]
-    })
-    .compileComponents();
+      imports: [DeleteCardComponent],
+      providers: [
+        provideMockStore(),
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            snapshot: {
+              params: { id: '123' },
+            },
+          },
+        },
+        { provide: Router, useValue: { navigate: jest.fn() } },
+        {
+          provide: ToastrService,
+          useValue: { success: jest.fn(), error: jest.fn() },
+        },
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(DeleteCardComponent);
     component = fixture.componentInstance;
+    debugElement = fixture.debugElement;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render the title and message', () => {
+    const titleElement = debugElement.query(
+      By.css('app-text[type="emphasis"]')
+    ).nativeElement;
+    const messageElement = debugElement.query(
+      By.css('app-text[size="1.3rem"]')
+    ).nativeElement;
+
+    expect(titleElement.textContent).toContain('Confirm Deletion');
+    expect(messageElement.textContent).toContain(
+      'Are you sure you want to delete invoice #'
+    );
   });
 });
