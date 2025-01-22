5d051aadb3e9a1a97a358e0e20c23c0929963ed5
Michael Darko <michael.darko@amalitech.com>
2025-01-22 10:27:20 +0000

    feat(app): implement token validation

--- week_7/invoice-app/src/app/auth/auth-interceptor.interceptor.ts
+++ week_7/invoice-app/src/app/auth/auth-interceptor.interceptor.ts
@@ -1,14 +1,27 @@
 import { HttpInterceptorFn } from '@angular/common/http';
+import { catchError, throwError } from 'rxjs';
+import { inject } from '@angular/core';
+import { Router } from '@angular/router';
 
 export const authInterceptorInterceptor: HttpInterceptorFn = (
   request,
   next
 ) => {
   const token = localStorage.getItem('authToken') ?? '';
+  const router = inject(Router);
+
   request = request.clone({
     setHeaders: {
       Authorization: `Bearer ${token}`,
     },
   });
-  return next(request);
+  return next(request).pipe(
+    catchError((error) => {
+      if (error.status === 403) {
+        localStorage.removeItem('authToken');
+        router.navigate(['/login']);
+      }
+      return throwError(error);
+    })
+  );
 };
