5a5931fe41458b21dcbdac0449eb48061aa7c401
Michael Darko <michael.darko@amalitech.com>
2025-01-22 10:57:34 +0000

    feat(app): implement logout functionality

--- week_7/invoice-app/src/app/components/avatar/avatar.component.ts
+++ week_7/invoice-app/src/app/components/avatar/avatar.component.ts
@@ -1,13 +1,28 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../auth/auth.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-avatar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './avatar.component.html',
   styleUrl: './avatar.component.css',
 })
 export class AvatarComponent {
+  private readonly router = inject(Router);
+  private readonly authService = inject(AuthService);
+  isLoggingOut!: boolean;
   @Input() width!: number;
   @Input() height!: number;
+
+  handlelogout(): void {
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
+
+  handleToggleDropdown(): void {
+    this.isLoggingOut = !this.isLoggingOut;
+  }
 }
