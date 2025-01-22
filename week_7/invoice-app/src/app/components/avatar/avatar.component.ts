26f714d7b52b143370c57388f7bb92cf7b584122
Michael Darko <michael.darko@amalitech.com>
2025-01-22 11:42:02 +0000

    feat(unauth): implement responsiveness to unauthorize screen

--- week_7/invoice-app/src/app/components/avatar/avatar.component.ts
+++ week_7/invoice-app/src/app/components/avatar/avatar.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, inject } from '@angular/core';
+import { Component, ElementRef, Input, Renderer2, inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../../auth/auth.service';
 import { CommonModule } from '@angular/common';
@@ -13,9 +13,19 @@ import { CommonModule } from '@angular/common';
 export class AvatarComponent {
   private readonly router = inject(Router);
   private readonly authService = inject(AuthService);
+  private readonly renderer = inject(Renderer2);
+  private readonly el = inject(ElementRef);
   isLoggingOut!: boolean;
   @Input() width!: number;
   @Input() height!: number;
+
+  constructor() {
+    this.renderer.listen('window', 'click', (e: Event) => {
+      if (!this.el.nativeElement.contains(e.target)) {
+        this.isLoggingOut = false;
+      }
+    });
+  }
 
   handlelogout(): void {
     this.authService.logout();
